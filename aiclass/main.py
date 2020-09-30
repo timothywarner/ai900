92f2926b70a0177bc6e8fd5a1c14f24e66f571c3
Tim Warner <tim@timw.info>
2020-09-30 10:33:45 -0500

    Add resources for Sept 2020 delivery

--- aiclass/main.py
+++ aiclass/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# AI-900 class September 2020
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 
